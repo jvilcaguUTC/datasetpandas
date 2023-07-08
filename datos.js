6c2df2fda9015eca05df59538c48b7c480625514
Jonathan Vilcaguano <vilcaguano.jonathan@gmail.com>
2023-07-08 13:49:08 -0500

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,58 @@
+[
+   {
+      "alumno":"Jonathan",
+      "nota":9.09,
+      "ranking":10
+   },
+   {
+      "alumno":"Juan",
+      "nota":3.81,
+      "ranking":150
+
+   },
+   {
+      "alumno":"Moises",
+      "nota":3,
+      "ranking":410
+
+   },
+   {
+      "alumno":"Leonel",
+      "nota":4.66,
+      "ranking":35
+
+   },
+   {
+      "alumno":"Ricardo",
+      "nota":5.25,
+      "ranking":185
+
+   },
+   {
+      "alumno":"Carlos",
+      "nota":3.14,
+      "ranking":175
+
+   },
+   {
+      "alumno":"Juan Pablo",
+      "nota":4.96,
+      "ranking":192
+
+   },
+   {
+      "alumno":"Cristina",
+      "nota":2.92,
+      "ranking":282
+   },
+   {
+      "alumno":"Marcos",
+      "nota":3.33,
+      "ranking":57
+   },
+   {
+      "alumno":"Cecilia",
+      "nota":4.94,
+      "ranking":12
+   }
+]
